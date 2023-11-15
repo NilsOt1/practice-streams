55678bba1804e3113e2dc90f53a0a36732362b73
nils <nilsotto@gmx.de>
2023-11-15 19:24:11 +0100

    add list of numbers and sort for even numbers

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,7 +1,20 @@
 package de.neuefische;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> listOfNumbers = List.of(5, 3, 4, 2, 9, 7, 8);
+
+        //Filter the list for even numbers:
+
+        List<Integer> evenNumbers = listOfNumbers.stream()
+                .filter(numbers -> numbers % 2 ==0)
+                .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+
 
     }
 }
